24cd442090e3a4db23010492bbe909ad95ebb22a
saikumar0612 <saikumar.basolutions@gmail.com>
2019-11-13 14:40:49 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   jobPortalUrl: 'https://tedpros.com/jobportal',
-  apiUrl: 'http://162.254.209.129/accountadmin',
+  // apiUrl: 'http://162.254.209.129/accountadmin',
+   apiUrl: 'http://localhost/tedpro_new_apis',
   superUrl: 'http://142.93.221.56/superadmin/api'
 };
 
